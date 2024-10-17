38f54ed363bbe8565dcf402257287aaed6b03c35
gsggsfsasdsdasd <168271459+gsggsfsasdsdasd@users.noreply.github.com>
2024-10-17 22:50:50 +0700

    Update home.jsx
    sửa trang home 17/10

--- src/components/sinhvien/home/home.jsx
+++ src/components/sinhvien/home/home.jsx
@@ -17,38 +17,54 @@ const Home = () => {
             <Navbar MaSV={MaSV} /> {/* Truyền MaSV cho Navbar */}
 
             <main className="main-content">
-                <section className="student-info">
-                    <h2 >Thông Tin Sinh Viên</h2>
-                    <p><strong>Họ tên:</strong> Trần Huỳnh Bảo Ngọc</p>
-                    <p><strong>MSSV:</strong> 2254810194</p>
-                    <p><strong>Địa chỉ:</strong> 18A/1 Cộng Hòa, phường 5, quận Tân Bình, TP.HCM</p>
-                    <p><strong>Trạng thái:</strong> Đang học</p>
-                    
-                </section>
+                     <div className="avatar">
+                       <img src="avatar_home.png" alt="ADD" />
+                     </div> 
+                    <div className="card">
+                   <div className="student-info">
+                       <h2 >Thông Tin Sinh Viên</h2>
+                       <p><strong>Họ tên:</strong> Trần Huỳnh Bảo Ngọc</p>
+                       <p><strong>MSSV:</strong> 2254810194</p>
+                       <p><strong>Địa chỉ:</strong> 18A/1 Cộng Hòa, phường 5, quận Tân Bình, TP.HCM</p>
+                       <p><strong>Trạng thái:</strong> Đang học</p>
+                    </div> 
+                </div>    
 
                 <div className="content-layout"> {/* Khung bố trí cho thời khóa biểu và biểu đồ */}
-                    <section className="schedule">
-                        <h2>Thời Khóa Biểu</h2>
+                    <div className="schedule">
+                        <div class="section-header">
+                            <h2>THỜI KHÓA BIỂU</h2>
+                            <a href="#">Xem chi tiết</a>
+                        </div>
+
                         <div className="schedule-grid">
                             <div className="schedule-item">
                                 <p>Thứ 2</p>
                                 <p>Hệ Quản Trị Cơ Sở Dữ Liệu</p>
+                                <p>Tiết 1 - 4 (7:00 - 10:35)</p>
                                 <p>Phòng: G305</p>
                                 <p>GV: Nguyễn Lương Anh Tuấn</p>
                             </div>
                             <div className="schedule-item">
                                 <p>Thứ 4</p>
                                 <p>Hệ Quản Trị Cơ Sở Dữ Liệu</p>
+                                <p>Tiết 1 - 4 (7:00 - 10:35)</p>
                                 <p>Phòng: G305</p>
                                 <p>GV: Nguyễn Lương Anh Tuấn</p>
                             </div>
-                           
-                            
+                            <div className="schedule-item">
+                                <p>Thứ 4</p>
+                                <p>Hệ Quản Trị Cơ Sở Dữ Liệu</p>
+                                <p>Tiết 1 - 4 (7:00 - 10:35)</p>
+                                <p>Phòng: G305</p>
+                                <p>GV: Nguyễn Lương Anh Tuấn</p>
+                            </div>
                         </div>
-                    </section>
+                    </div>
 
                     <section className="course-progress">
                         <h2>Học Phần</h2>
+                        <a href="#">Xem chi tiết</a>
                         <div className="progress-container">
                             <div className="progress-circle">
                                 <span>33%<br />Chưa Học</span>
@@ -57,6 +73,7 @@ const Home = () => {
                                 <span>67%<br />Đã Học</span>
                             </div>
                         </div>
+                        
                     </section>
                 </div>
             </main>
