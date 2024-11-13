32a0e89d2b5543c8b470c24d0d1f16838dc34842
huybaoo <156101571+huybaoo@users.noreply.github.com>
2024-11-13 10:50:02 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,24 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const svLoginRoutes = require('./routes/sv_login');
 const sinhVienRoutes = require('./routes/sinhvien');
+const db = require('./db'); // Import db connection
 
 // Khởi tạo ứng dụng
 const app = express();
+
+// Sử dụng middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+// Kiểm tra kết nối database
+db.connect(err => {
+    if (err) {
+        console.error('Lỗi kết nối tới cơ sở dữ liệu:', err);
+        process.exit(1);
+    } else {
+        console.log('Kết nối tới cơ sở dữ liệu thành công!');
+    }
+});
 
 // Định tuyến cho các API
 app.use('/api/login', svLoginRoutes); // Đường dẫn cho đăng nhập sinh viên
