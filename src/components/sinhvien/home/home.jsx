937da39e3835c6ee3409518530aa08f01c3d78a0
gsggsfsasdsdasd <168271459+gsggsfsasdsdasd@users.noreply.github.com>
2024-11-21 09:14:02 +0700

    Update home.jsx

--- src/components/sinhvien/home/home.jsx
+++ src/components/sinhvien/home/home.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './home.css';
 import Navbar from '../navbar/navbar'; 
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Home = () => {
     const [MaSV, setMaSV] = useState('');
@@ -11,6 +15,29 @@ const Home = () => {
             setMaSV(storedMaSV); // Lưu MaSV vào state
         }
     }, []);
+
+
+    //du lieu dât cho bieu do
+    const data = {
+        labels: ['Chưa Học', 'Đã Học'], // Nhãn cho biểu đồ
+        datasets: [
+            {
+                data: [33, 67], // Giá trị tỷ lệ phần trăm
+                backgroundColor: ['#3498db', '#2ecc71'], // Màu sắc từng phần
+                borderColor: ['#ffffff', '#ffffff'], // Màu viền
+                borderWidth: 2, // Độ dày viền
+            },
+        ],
+    };
+
+    const options = {
+        plugins: {
+            legend: {
+                display: true,
+                position: 'bottom', // Vị trí hiển thị chú thích
+            },
+        },
+    };
 
     return (
         <div className="home"> {/* Sử dụng class home để áp dụng CSS */}
@@ -39,7 +66,7 @@ const Home = () => {
             <div className="schedule">
                 <div className="section-header">
                     <h2>THỜI KHÓA BIỂU</h2>
-                    <a href="#">Xem chi tiết</a>
+                    <a href="/thoikhoabieu">Xem chi tiết</a>
                 </div>
 
                 <div className="schedule-grid">
@@ -71,12 +98,7 @@ const Home = () => {
                 <h2>Học Phần</h2>
                 <a href="#">Xem chi tiết</a>
                 <div className="progress-container">
-                    <div className="progress-circle">
-                        <span>33%<br />Chưa Học</span>
-                    </div>
-                    <div className="progress-circle">
-                        <span>67%<br />Đã Học</span>
-                    </div>
+                <Doughnut data={data} options={options} />
                 </div>
             </section>
         </div>
