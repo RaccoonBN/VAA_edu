02291806436d823f26e57baefeacd26c652b817f
GiaKiet2412 <giakiet241204@gmail.com>
2024-11-23 11:25:38 +0700

    Update dangkyhp.jsx

--- src/components/sinhvien/dangkyhp/dangkyhp.jsx
+++ src/components/sinhvien/dangkyhp/dangkyhp.jsx
@@ -3,17 +3,13 @@ import './dangkyhp.css';
 import Navbar from '../navbar/navbar';
 
 const Dangkyhp = () => {
-  const defaultSinhVien = {
-    HoTen: 'Trần Huỳnh Bảo Ngọc',
-    Avatar: require('../../../img/avatar_default.jpg'), // Đảm bảo đường dẫn này chính xác
-  };
-
-  const [sinhVien, setSinhVien] = useState(defaultSinhVien);
+  const [sinhVien, setSinhVien] = useState(null);
   const [courses, setCourses] = useState([]);
   const [selectedSemester, setSelectedSemester] = useState('');
 
   useEffect(() => {
     const storedSinhVien = JSON.parse(localStorage.getItem('sinhVien'));
+    console.log('Stored sinhVien:', storedSinhVien); // Debug thông tin sinh viên
     if (storedSinhVien) {
       setSinhVien(storedSinhVien);
     } else {
@@ -36,7 +32,7 @@ const Dangkyhp = () => {
   }, [selectedSemester]);
 
   const handleRegister = async (course) => {
-    if (!sinhVien.id) {
+    if (!sinhVien || !sinhVien.MaSV) { // Kiểm tra MaSV
       alert('Vui lòng đăng nhập để đăng ký học phần!');
       return;
     }
@@ -50,8 +46,8 @@ const Dangkyhp = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ 
-          studentId: sinhVien.id, 
-          ngaydangky: currentDate // Gửi ngày đăng ký
+          studentId: sinhVien.MaSV, // Sử dụng MaSV
+          ngaydangky: currentDate 
         }),
       });
 
@@ -60,7 +56,7 @@ const Dangkyhp = () => {
         setCourses(courses.map(c => c.MaHP === course.MaHP ? { ...c, sisohientai: c.sisohientai + 1 } : c));
       } else {
         const errorData = await response.json();
-        alert(`Đăng ký học phần thất bại: ${errorData.error}`);
+        alert(`${errorData.error}`);
       }
     } catch (error) {
       console.error('Lỗi khi đăng ký học phần:', error);
@@ -98,6 +94,8 @@ const Dangkyhp = () => {
                 <h3>{course.tenHP}</h3>
                 <p>Giảng viên: {course.giangvien}</p>
                 <p>Phòng: {course.phonghoc}</p>
+                <p>Ngày học: {course.lichhoc}</p>
+                <p>Giờ học: {course.giobatdaubuoihoc} - {course.gioketthucbuoihoc}</p>
                 <p>Sĩ số: {course.sisohientai}/{course.siso}</p>
                 <button
                   className="dkhpregister-button"
